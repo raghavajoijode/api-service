c97e49d10f2b8355334ab9cde9b79228b14ed08d
Raghava Joijode <raghava.joijode@gmail.com>
2023-08-17 19:24:35 +0530

    Added basic Login, Registration APIs

--- src/app/routes/api.js
+++ src/app/routes/api.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import otpRouter from '../services/otp/index.js';
 import notFoundRouter from '../services/404/index.js'
+import authRouter from '../services/auth/index.js'
 
 
 const router = Router()
 
 router
     .use('/otp', otpRouter)
+    .use('/auth', authRouter)
     // Add any other routes
     .use('*', notFoundRouter)
 
